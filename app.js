555b3e3ed54fcfa2c1df6ea6b9c351849e00ab6f
rajupalumaru <raju.palumaru94@gmail.com>
2024-03-26 20:49:23 +0000

    added all apis

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const app = express()
-
+app.use(express.json())
 const dbPath = path.join(__dirname, 'cricketTeam.db')
 
 let db = null
@@ -52,3 +52,50 @@ app.get('/players/:playerId/', async (request, response) => {
   const player = await db.get(getPlayerQuery)
   response.send(convertDbObjectToResponseObject(player))
 })
+
+//create a new player
+
+app.post('/players/', async (request, response) => {
+  const playerDetails = request.body
+  const {player_Name, jersey_Number, role} = playerDetails
+  const addPlayerQuery = `
+    Insert into cricket_team (player_name,jersey_number,role) 
+    values (
+        '${player_Name}',
+        ${jersey_Number},
+        '${role}'
+    )`
+  const dbResponse = await db.run(addPlayerQuery)
+  const playerId = dbResponse.lastID
+  response.send('Player Added to Team')
+})
+
+//update player
+
+app.put('/players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const playerDetails = request.body
+  const {player_Name, jersey_Number, role} = playerDetails
+
+  const updatePlayerQuery = `
+  Update 
+  cricket_team 
+  Set 
+    player_name : '${player_Name}',
+    jersey_number :${jersey_Number},
+    role : '${role}'
+    Where player_id = ${playerId}`
+  await db.run(updatePlayerQuery)
+  response.send('Player Details Updated')
+})
+
+// delete book APi
+app.delete('/players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const deletePlayerQuery = `
+    Delete from cricket_team Where player_id = ${playerId}`
+  await db.run(deletePlayerQuery)
+  response.send('Player Removed')
+})
+
+module.exports = app
